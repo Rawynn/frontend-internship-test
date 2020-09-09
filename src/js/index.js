026d9dd07afb7ca097947dcf06cff46cf3db30d1
Rawynn <rawynn@gmail.com>
2020-09-09 10:04:06 +0200

    Task1: popup form, close button

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
 /* Here goes your JS code */
+$(document).ready(function () {
+    $('#show-popup-form').on("click", function () {
+        $(this).css("opacity", "0");
+        $('.popup').css("animation", "down 1s forwards .5s");
+    });
+
+    $('.close-btn').on("click", function () {
+        $('.popup').css("animation", "up 1s forwards");
+        setTimeout(function () {
+            $('#show-popup-form').css("opacity", "1");
+        }, 600);
+
+    });
+});
